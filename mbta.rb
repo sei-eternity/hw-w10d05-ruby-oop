73e9695ecdd4041af5dbd9dbf2e5d9575fab329c
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-03-27 21:03:26 +0300

    Done

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,49 @@
-class Subway
+class Subway 
+   
+    def initialize
+        @subway_lines = {
+            Red: Line.new("Red",[Station.new("South Station"),
+            Station.new("Park Street"),
+            Station.new("Kendall"),
+            Station.new("Central"),
+            Station.new("Harvard"),
+            Station.new("Porter"),
+            Station.new("Davis"),
+            Station.new("Alewife")]),
+            Green: Line.new("Green",[Station.new("Government Center"),
+            Station.new("Park Street"),
+            Station.new("Boylston"),
+            Station.new("Arlington"),
+            Station.new("Copley"),
+            Station.new("Hynes"),
+            Station.new("Kenmore")]),
+            Orange: Line.new("Orange",[Station.new("North Station"),
+            Station.new("Haymarket"),
+            Station.new("Park Street"),
+            Station.new("State"),
+            Station.new("Downtown Crossing"),
+            Station.new("Chinatown"),
+            Station.new("Back Bay"),
+            Station.new("Forest Hills")])
+    }
+end
+     
     def stops_between_stations(start_line, start_station, end_line, end_station)
+        if start_line==end_line
+            # if the user in same line
+            if start_station==end_station
+                # if the user in same station
+
+               return  puts 0
+            end
+            #Else but stell in same Line  
+            #Prent the number of satation by sub stations index
+            return puts( @subway_lines[start_line.to_sym].prent_stations.index(start_station)- @subway_lines[start_line.to_sym].prent_stations.index(end_station).abs)
+            
+         end
+         #Else so satation not in same line as we know Park Street is intersection point
+         return puts (@subway_lines[start_line.to_sym].prent_stations.index(start_station) - @subway_lines[start_line.to_sym].prent_stations.index("Park Street")).abs + (@subway_lines[end_line.to_sym].prent_stations.index("Park Street") - @subway_lines[end_line.to_sym].prent_stations.index(end_station)).abs
+         
     end
   end
     
@@ -10,8 +54,10 @@ class Subway
         @station_array =stations
      end
      def prent_stations
-        @station_array.each{|station| 
-    puts station.prent_name}
+        
+     @station_array.map{|station| 
+     station.prent_name}
+    
      end
   end
   
@@ -33,39 +79,44 @@ class Subway
 #   porter=Station.new("Porter")
 #   davis=Station.new("Davis")
 #   alewife=Station.new("Alewife")
-  red_array=[Station.new("South Station"),
-  Station.new("Park Street"),
-  Station.new("Kendall"),
-  Station.new("Central"),
-  Station.new("Harvard"),
-  Station.new("Porter"),
-  Station.new("Davis"),
-  Station.new("Alewife")]
+#   red_array=[Station.new("South Station"),
+#   Station.new("Park Street"),
+#   Station.new("Kendall"),
+#   Station.new("Central"),
+#   Station.new("Harvard"),
+#   Station.new("Porter"),
+#   Station.new("Davis"),
+#   Station.new("Alewife")]
 
-  green_array=[Station.new("Government Center"),
-  Station.new("Park Street"),
-  Station.new("Boylston"),
-  Station.new("Arlington"),
-  Station.new("Copley"),
-  Station.new("Hynes"),
-  Station.new("Kenmore")]
+#   green_array=[Station.new("Government Center"),
+#   Station.new("Park Street"),
+#   Station.new("Boylston"),
+#   Station.new("Arlington"),
+#   Station.new("Copley"),
+#   Station.new("Hynes"),
+#   Station.new("Kenmore")]
 
-  orange_array=[Station.new("North Station"),
-  Station.new("Haymarket"),
-  Station.new("Park Street"),
-  Station.new("State"),
-  Station.new("Downtown Crossing"),
-  Station.new("Chinatown"),
-  Station.new("Back Bay"),
-  Station.new("Forest Hills")]
+#   orange_array=[Station.new("North Station"),
+#   Station.new("Haymarket"),
+#   Station.new("Park Street"),
+#   Station.new("State"),
+#   Station.new("Downtown Crossing"),
+#   Station.new("Chinatown"),
+#   Station.new("Back Bay"),
+#   Station.new("Forest Hills")]
 
   
 
-  subway_lines = {
-          Red: Line.new("Red",red_array),
-          Green: Line.new("Green",green_array),
-          Orange: Line.new("Orange",orange_array)
-  }
-  subway_lines[:Red].prent_stations
-  subway_lines[:Green].prent_stations
-  subway_lines[:Orange].prent_stations
+#   subway_lines = {
+#           Red: Line.new("Red",red_array),
+#           Green: Line.new("Green",green_array),
+#           Orange: Line.new("Orange",orange_array)
+#   }
+#   subway_lines[:Red].prent_stations
+#   subway_lines[:Green].prent_stations
+#   subway_lines[:Orange].prent_stations
+  puts "+++++++++++++"
+  mbta = Subway.new
+  mbta.stops_between_stations('Red', 'Alewife', 'Red', 'Alewife') # 0
+  mbta.stops_between_stations('Red', 'Alewife', 'Red', 'South Station') # 7
+  mbta.stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') # 6
